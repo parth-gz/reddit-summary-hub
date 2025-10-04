e739bcd84ddfb1e08241bf7a6dd07469bed1cdac
parth-gz <gaikwadparth2304@gmail.com>
2025-10-05 00:20:44 +0530

    deleted file

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDK89HiVC0g85YYIgYDww377Gmwa6EnkTs")
-
-models = genai.list_models()
-
-for model in models:
-    print(model.name)
